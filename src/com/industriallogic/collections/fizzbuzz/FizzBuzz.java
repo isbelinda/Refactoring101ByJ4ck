b80309f8f27e51acc4f5a3c6a1fa1c02ac66e626
Belinda <bow@socket9.com>
2016-11-19 14:19:42 +0700

    Refactor function

--- src/com/industriallogic/collections/fizzbuzz/FizzBuzz.java
+++ src/com/industriallogic/collections/fizzbuzz/FizzBuzz.java
@@ -10,17 +10,29 @@ public class FizzBuzz {
     public static final String FIZZ_BUZZ = "FizzBuzz";
 
     public String answer(int number){
-        if(number % 3 == 0 && number % 5 == 0){
+        if(isFizzBuzz(number)){
             return FIZZ_BUZZ;
         }
-        if(number % 3 == 0){
+        if(isFizz(number)){
             return FIZZ;
         }
 
-        if(number % 5 == 0){
+        if(isBuzz(number)){
             return BUZZ;
         }
 
         return String.valueOf(number);
+    }
+
+    private boolean isFizzBuzz(int number) {
+        return isFizz(number) && isBuzz(number);
+    }
+
+    private boolean isBuzz(int number) {
+        return number % 5 == 0;
+    }
+
+    private boolean isFizz(int number) {
+        return number % 3 == 0;
     }
 }
